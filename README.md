219724e7573d243442e5c37e9d4279513aa71378
Kyle <kwill036@plattsburgh.edu>
2015-07-23 05:16:19 -0400

    introduced first working file to the repository

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tkinter-Fun
 Python Projects Involving Tkinter
+
+Here is a repository for a fun little project that I wanted to make using Python's Tkinter GUI package.
---
+++ pythonproj.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from tkinter import Tk, Frame, Button, Toplevel, Canvas
+import random
+
+class Frame1(Frame):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        but = Button(parent, text="New Window", command=self.newWindow)
+        but.place(relx=0.25, rely=0.5)
+
+    def newWindow(self):
+        random.seed()
+        colorInt = random.randint(0,2)
+        if colorInt == 0:
+            bgColor = "red"
+        elif colorInt == 1:
+            bgColor = "white"
+        else:
+            bgColor = "blue"
+        top = Toplevel()
+        top.title("New!")
+        top.configure(background=bgColor)
+        but = Button(top, text="New Window", command=self.newWindow)
+        but.place(relx=0.25, rely=0.5)
+        #square = Square(top)
+
+class Square():
+    def __init__(self, parent):
+        self.parent = parent
+        can = Canvas(parent, width=100, height=100)
+
+def main():
+    rootWindow = Tk()
+    rootWindow.title("Congratulations!")
+    rootWindow.minsize(width=200, height=200)
+    rootWindow.configure(background='blue')
+    app = Frame1(rootWindow)
+
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
